d8c102d05623a3b42c4db5e484af6c07940bb70b
Andre Balconi <andrebalconi@hotmail.com>
2021-03-26 14:58:34 -0300

    Add routes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { useScrolltoTop } from "hooks/scroll";
+
 import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
 import Section from "components/molecules/Section";
 import Grid from "components/atoms/Grid";
 import Footer from "components/organisms/Footer";
 import Card, { CardMedia, CardMediaDescription } from "components/atoms/Card";
+import BreadCrumb from "components/atoms/BreadCrumb";
 
 import HeroImage from "assets/hero.jpg";
 import AboutImage from "draws/About";
@@ -36,101 +39,109 @@ const instructors = [
   { id: 6, name: "Bob", avatar: InstImage6 },
 ];
 
-const About = () => (
-  <>
-    <Hero image={HeroImage}>
-      <Heading>
-        <h1>Auto Escola Nardini</h1>
-      </Heading>
-    </Hero>
-    <Section>
-      <Grid md={2}>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum
-            quisque non tellus orci.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. In nisl
-            nisi scelerisque eu ultrices vitae. Metus dictum at tempor commodo
-            ullamcorper.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Suspendisse ultrices gravida dictum fusce ut placerat orci.
-          </p>
-          <ul>
-            <li>
-              Laoreet suspendisse interdum consectetur libero id faucibus nisl.
-            </li>
-            <li>Faucibus a pellentesque sit amet porttitor eget dolor. </li>
-            <li>
-              Leo duis ut diam quam. Mauris in aliquam sem fringilla ut morbi.{" "}
-            </li>
-            <li>
-              Duis at tellus at urna. Massa tempor nec feugiat nisl pretium
-              fusce id velit.
-            </li>
-            <li>
-              Vel elit scelerisque mauris pellentesque pulvinar pellentesque
-              habitant.
-            </li>
-          </ul>
-        </div>
-        <div>
-          <p>
-            Lectus urna duis convallis convallis tellus id interdum. Urna neque
-            viverra justo nec ultrices dui sapien eget mi. Ullamcorper sit amet
-            risus nullam eget felis eget nunc lobortis.
-          </p>
-          <center>
-            <ImageContainer>
-              <AboutImage />
-            </ImageContainer>
-          </center>
-        </div>
-      </Grid>
-    </Section>
-    <Section inverse>
-      <Grid md={2}>
-        <div>
-          <h4>Missao</h4>
-          <p>
-            Mauris in aliquam sem fringilla ut morbi. Duis at tellus at urna.
-            Consequat id porta nibh venenatis.
-          </p>
-        </div>
-        <div>
-          <h4>Visao</h4>
-          <p>
-            Massa tempor nec feugiat nisl pretium fusce id velit. Consequat id
-            porta nibh venenatis.
-          </p>
-        </div>
-      </Grid>
-    </Section>
-    <Section>
-      <Heading>
-        <h2>Conheca nossos professores</h2>
-      </Heading>
-      <Grid sm={2} md={3} lg={4}>
-        {instructors.map((instructor) => (
-          <Card key={instructor.id}>
-            <CardMedia image={instructor.avatar}>
-              <CardMediaDescription>
-                <h5>{instructor.name}</h5>
-              </CardMediaDescription>
-            </CardMedia>
-          </Card>
-        ))}
-      </Grid>
-    </Section>
-    <Footer />
-  </>
-);
+const About = () => {
+  useScrolltoTop();
+
+  return (
+    <>
+      <Hero image={HeroImage}>
+        <Heading>
+          <h1>Auto Escola Nardini</h1>
+        </Heading>
+        <BreadCrumb
+          items={[{ label: "Inicio", link: "/" }, { label: "Sobre" }]}
+        />
+      </Hero>
+      <Section>
+        <Grid md={2}>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Rutrum
+              quisque non tellus orci.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+              nisl nisi scelerisque eu ultrices vitae. Metus dictum at tempor
+              commodo ullamcorper.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Suspendisse ultrices gravida dictum fusce ut placerat orci.
+            </p>
+            <ul>
+              <li>
+                Laoreet suspendisse interdum consectetur libero id faucibus
+                nisl.
+              </li>
+              <li>Faucibus a pellentesque sit amet porttitor eget dolor. </li>
+              <li>
+                Leo duis ut diam quam. Mauris in aliquam sem fringilla ut morbi.{" "}
+              </li>
+              <li>
+                Duis at tellus at urna. Massa tempor nec feugiat nisl pretium
+                fusce id velit.
+              </li>
+              <li>
+                Vel elit scelerisque mauris pellentesque pulvinar pellentesque
+                habitant.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              Lectus urna duis convallis convallis tellus id interdum. Urna
+              neque viverra justo nec ultrices dui sapien eget mi. Ullamcorper
+              sit amet risus nullam eget felis eget nunc lobortis.
+            </p>
+            <center>
+              <ImageContainer>
+                <AboutImage />
+              </ImageContainer>
+            </center>
+          </div>
+        </Grid>
+      </Section>
+      <Section inverse>
+        <Grid md={2}>
+          <div>
+            <h4>Missao</h4>
+            <p>
+              Mauris in aliquam sem fringilla ut morbi. Duis at tellus at urna.
+              Consequat id porta nibh venenatis.
+            </p>
+          </div>
+          <div>
+            <h4>Visao</h4>
+            <p>
+              Massa tempor nec feugiat nisl pretium fusce id velit. Consequat id
+              porta nibh venenatis.
+            </p>
+          </div>
+        </Grid>
+      </Section>
+      <Section>
+        <Heading>
+          <h2>Conheca nossos professores</h2>
+        </Heading>
+        <Grid sm={2} md={3} lg={4}>
+          {instructors.map((instructor) => (
+            <Card key={instructor.id}>
+              <CardMedia image={instructor.avatar}>
+                <CardMediaDescription>
+                  <h5>{instructor.name}</h5>
+                </CardMediaDescription>
+              </CardMedia>
+            </Card>
+          ))}
+        </Grid>
+      </Section>
+      <Footer />
+    </>
+  );
+};
 
 About.defaultProps = {};
 
--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -17,6 +17,9 @@ import Callout, {
 
 import HeroImage from "assets/hero.jpg";
 import SpeedImage from "draws/Speed";
+import BreadCrumb from "components/atoms/BreadCrumb";
+
+import { useScrolltoTop } from "hooks/scroll";
 
 const PinnedList = styled.ul`
   list-style: none;
@@ -36,68 +39,78 @@ const PinnedItem = styled.li`
   }
 `;
 
-const ProductDetail = () => (
-  <>
-    <Hero image={HeroImage}>
-      <Heading>
-        <h1>Nome do Servico</h1>
-      </Heading>
-    </Hero>
-    <Section>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Tellus in hac
-        habitasse platea dictumst vestibulum rhoncus. Lacus laoreet non
-        curabitur gravida arcu ac.
-      </p>
-      <p>
-        Semper viverra nam libero justo laoreet sit amet cursus sit.
-        Pellentesque habitant morbi tristique senectus et. Imperdiet proin
-        fermentum leo vel orci porta non pulvinar neque.{" "}
-      </p>
-      <p>
-        Sem fringilla ut morbi tincidunt. Risus sed vulputate odio ut enim. Nibh
-        ipsum consequat nisl vel pretium lectus. Amet facilisis magna etiam
-        tempor.
-      </p>
-      <h5>Documentos necessarios</h5>
-      <PinnedList>
-        <PinnedItem>
-          <FaIdCard />
-          RG
-        </PinnedItem>
-        <PinnedItem>
-          <FaIdCard />
-          CPF
-        </PinnedItem>
-        <PinnedItem>
-          <FaScroll />
-          Certidao de nascimento ou casamento
-        </PinnedItem>
-        <PinnedItem>
-          <FaHome />
-          Comprovante de Residencia
-        </PinnedItem>
-      </PinnedList>
-    </Section>
-    <Section inverse>
-      <Callout>
-        <CalloutBody>
-          <h6>Faca a sua matricula agora mesmo</h6>
-          <p>Qualquer texto somente para testar como esta ficando</p>
-          <CallautActions>
-            <Button color="primary">Matricular</Button>
-          </CallautActions>
-        </CalloutBody>
-        <CalloutMedia>
-          <SpeedImage />
-        </CalloutMedia>
-      </Callout>
-    </Section>
-    <Footer />
-  </>
-);
+const ProductDetail = () => {
+  useScrolltoTop();
 
+  return (
+    <>
+      <Hero image={HeroImage}>
+        <Heading>
+          <h1>Nome do Servico</h1>
+        </Heading>
+        <BreadCrumb
+          items={[
+            { label: "Inicio", link: "/" },
+            { label: "Servicos" },
+            { label: "Nome do Servico" },
+          ]}
+        />
+      </Hero>
+      <Section>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus in
+          hac habitasse platea dictumst vestibulum rhoncus. Lacus laoreet non
+          curabitur gravida arcu ac.
+        </p>
+        <p>
+          Semper viverra nam libero justo laoreet sit amet cursus sit.
+          Pellentesque habitant morbi tristique senectus et. Imperdiet proin
+          fermentum leo vel orci porta non pulvinar neque.{" "}
+        </p>
+        <p>
+          Sem fringilla ut morbi tincidunt. Risus sed vulputate odio ut enim.
+          Nibh ipsum consequat nisl vel pretium lectus. Amet facilisis magna
+          etiam tempor.
+        </p>
+        <h5>Documentos necessarios</h5>
+        <PinnedList>
+          <PinnedItem>
+            <FaIdCard />
+            RG
+          </PinnedItem>
+          <PinnedItem>
+            <FaIdCard />
+            CPF
+          </PinnedItem>
+          <PinnedItem>
+            <FaScroll />
+            Certidao de nascimento ou casamento
+          </PinnedItem>
+          <PinnedItem>
+            <FaHome />
+            Comprovante de Residencia
+          </PinnedItem>
+        </PinnedList>
+      </Section>
+      <Section inverse>
+        <Callout>
+          <CalloutBody>
+            <h6>Faca a sua matricula agora mesmo</h6>
+            <p>Qualquer texto somente para testar como esta ficando</p>
+            <CallautActions>
+              <Button color="primary">Matricular</Button>
+            </CallautActions>
+          </CalloutBody>
+          <CalloutMedia>
+            <SpeedImage />
+          </CalloutMedia>
+        </Callout>
+      </Section>
+      <Footer />
+    </>
+  );
+};
 ProductDetail.defaultProps = {};
 
 ProductDetail.propTypes = {};
