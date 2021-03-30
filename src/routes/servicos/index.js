62636ad64cd4c8e9fa4977b87c74007800f70739
Andre Balconi <andrebalconi@hotmail.com>
2021-03-30 12:06:35 -0300

    Add Not Found Page

--- src/routes/servicos/index.js
+++ src/routes/servicos/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import ProductDetailPage from "components/pages/ProductDetail";
+import Error from "components/pages/Error";
+import NotFoundImage from "draws/About";
 
 import { useScrolltoTop } from "hooks/scroll";
 import { useProduct } from "hooks/products";
@@ -11,6 +13,16 @@ const ProductDetail = () => {
 
   const { slang } = useParams();
   const product = useProduct({ slang });
+
+  if (!product) {
+    return (
+      <Error
+        image={<NotFoundImage />}
+        title="Servico nao encontrado"
+        description="Servico nao encontrado ou nao disponivel"
+      />
+    );
+  }
 
   return <ProductDetailPage product={product} />;
 };
